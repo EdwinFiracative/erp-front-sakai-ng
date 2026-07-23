e19377d0a91344e35bdc04d39c8e68879e7024e6
Edwin Firacative <edwin.firacative@outlook.com>
2026-07-23 16:59:32 -0500

    feat: add Pedido model and service for managing order data

---
+++ src/app/services/pedido-service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { APP_SETTINGS } from '../../app.settings';
+
+import { Pedido } from '../../models/pedido';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PedidoService {
+    private http = inject(HttpClient);
+    private settings = inject(APP_SETTINGS);
+    private apiUrl = `${this.settings.apiUrl}/api/pedido/filter`;
+
+    listPedidos(): Observable<Pedido[]> {
+        return this.http.post<Pedido[]>(this.apiUrl, { num: ['22583', '15552'], tdespacho: ['URBAN'] });
+    }
+}
---
+++ src/models/pedido.ts
@@ -0,0 +1,26 @@
+export interface Pedido {
+	id: number;
+	destinat?: string;
+	tdespacho?: string;
+	num?: string;
+	cliente?: string;
+	fecha?: string;
+	cod?: string;
+	nom?: string;
+	ud?: string;
+	cant?: number;
+	costo?: number;
+	pend?: number;
+	estado?: string;
+	desp?: number;
+	desc3?: number;
+	zona?: string;
+	vendedor?: string;
+	entrega?: string;
+	orden?: string;
+	ncod?: string;
+	fechan?: string;
+	sys2015?: string;
+	detalle?: string;
+	condic?: string;
+}
